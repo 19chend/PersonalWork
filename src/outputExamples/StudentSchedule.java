7b702638fdd2c1fb44fc904f809fb61af42c944c
Daisy <19chend@elmbrookstudents.org>
2018-09-13 13:43:57 -0500

    Student Schedule

---
+++ src/outputExamples/StudentSchedule.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class StudentSchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Daisy Chen");
+System.out.println("Student Schedule:");
+System.out.println("Block 1: Sculpture and Ceramics 1");
+System.out.println("Block 2: Chamber Orchestra/Study Hall");
+System.out.println("Block 3: AP Biology");
+System.out.println("Block 4: AP Computer Science");
+	}
+
+}
