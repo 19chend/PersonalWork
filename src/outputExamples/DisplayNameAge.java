b9ef497a2a5ba951e1cb93cde867a2882cd03204
Daisy <19chend@elmbrookstudents.org>
2018-09-13 13:17:40 -0500

    DisplayNameAge

---
+++ src/outputExamples/DisplayNameAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class DisplayNameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Daisy");
+System.out.println("Chen");
+System.out.println(17);
+	}
+
+}
