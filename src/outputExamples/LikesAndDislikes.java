8c4f43d9ff27c14615a9b3ad807e7edbc87c0a0e
Daisy <19chend@elmbrookstudents.org>
2018-09-13 14:04:08 -0500

    Likes and Dislikes in Columns

---
+++ src/outputExamples/LikesAndDislikes.java
@@ -0,0 +1,22 @@
+package outputExamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Likes:");
+		System.out.println("\t \tDislikes:");
+		System.out.print("1.Dogs");
+		System.out.println("\t \t1.Bugs");
+		System.out.print("2.Biking");
+		System.out.println("\t2.Horror movies");
+		System.out.print("3.Comedy movies");
+		System.out.println("\t3.Rugby");
+		System.out.print("4.Pizza");
+		System.out.println("\t \t4.Bio");
+		System.out.print("5.Swimming");
+		System.out.println("\t5.Country music");
+	}
+	
+
+}
