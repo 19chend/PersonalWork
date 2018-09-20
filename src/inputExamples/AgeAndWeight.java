7d3870be386a0340f748aa1d2d917e4d66da80bd
Daisy <19chend@elmbrookstudents.org>
2018-09-20 13:29:25 -0500

    Display age and weight

---
+++ src/inputExamples/AgeAndWeight.java
@@ -0,0 +1,17 @@
+package inputExamples;
+import java.util.Scanner;
+public class AgeAndWeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+int age = 999;
+double weight = 999.0;
+System.out.println("What is your age and weight?");
+age = input.nextInt();
+weight = input.nextDouble();
+System.out.println("You are "+age+" years old and weigh "+weight +" pounds.");
+
+	}
+
+}
