7bb69fec5d0c08c2d5b31ce460bfd9e1059e04c7
Daisy <19chend@elmbrookstudents.org>
2018-09-21 14:57:08 -0500

    Wall area

---
+++ src/inputExamples/WallArea.java
@@ -0,0 +1,17 @@
+package inputExamples;
+import java.util.Scanner; 
+public class WallArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+int height = 8;
+int width = 999;
+int area = 999;
+System.out.println("What is the width in feet of the 8ft tall wall?");
+width = input.nextInt();
+area = height * width;
+System.out.println("The wall has an area of "+area+" ft squared.");
+	}
+
+}
