820f463d9a366de10e9bfe755fd755e43de4b8d3
Daisy <19chend@elmbrookstudents.org>
2018-10-02 14:58:24 -0500

    Neighbor commands exercise

---
+++ src/objectOrientedProgramming/NeighborCommandsTest.java
@@ -0,0 +1,15 @@
+package objectOrientedProgramming;
+
+public class NeighborCommandsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+NeighborCommands neighbor = new NeighborCommands();
+Friend friend = new Friend();
+neighbor.sayJoke();
+neighbor.sayMoney();
+neighbor.sayConversation();
+friend.sayMessage();
+	}
+
+}
