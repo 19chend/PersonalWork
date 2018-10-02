8ea301b3b722e61660813dea681e63d6b834eafd
Daisy <19chend@elmbrookstudents.org>
2018-10-02 14:34:18 -0500

    Car commands exercise

---
+++ src/objectOrientedProgramming/CarCommandTest.java
@@ -0,0 +1,17 @@
+package objectOrientedProgramming;
+import java.util.Scanner;
+public class CarCommandTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+int miles = -999;
+System.out.println("How many miles are on the car?");
+miles = input.nextInt();
+CarCommands car = new CarCommands();
+car.sayOdometer(miles);
+car.sayFlat();
+car.sayThirsty();
+	}
+
+}
