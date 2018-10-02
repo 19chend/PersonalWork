0e0a501fac2ed95d685a1ca880e2f164e9735ecc
Daisy <19chend@elmbrookstudents.org>
2018-10-01 19:16:31 -0500

    Car

--- src/objectOrientedProgramming/Friend.java
+++ src/objectOrientedProgramming/Friend.java
@@ -1,8 +1,14 @@
 package objectOrientedProgramming;
-
+//import statements at the top
 public class Friend {
-
+	//instance fields - data storage
+	
+	//constructors - methods for constructing the object
+		
+	//default constructor
 public Friend()
 {
+	//statements to initialize the state of the object
 }
+//methods - things the object can do
 }
