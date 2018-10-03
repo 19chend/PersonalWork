cfefa9554701a4a62123db538ecfd93c2589820f
Daisy <19chend@elmbrookstudents.org>
2018-10-03 14:48:07 -0500

    Room wall area exercise

---
+++ src/objectOrientedProgramming/RoomAreaTest.java
@@ -0,0 +1,12 @@
+package objectOrientedProgramming;
+import java.util.Scanner;
+public class RoomAreaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+RoomArea dimensions = new RoomArea();
+dimensions.area();
+dimensions.areaWithDoor();
+}
+
+}
