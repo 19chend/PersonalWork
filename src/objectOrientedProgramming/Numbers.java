7f4a65a150de007c661097ad31e7d1fdd879f59f
Daisy <19chend@elmbrookstudents.org>
2018-10-04 14:02:29 -0500

    Numbers exercise

---
+++ src/objectOrientedProgramming/Numbers.java
@@ -0,0 +1,18 @@
+package objectOrientedProgramming;
+
+public class Numbers {
+public Numbers(){
+	
+}
+public void sayNumber(double num){
+	System.out.println("Your number is "+num);
+}
+public void sayNumberPlus2(double num){
+	num = num+2;
+	System.out.println("Your number plus 2 is "+num);
+}
+public void saySum(double num1, double num2){
+System.out.println("The sum of your numbers is "+(num1+num2));
+}
+}
+
---
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -0,0 +1,14 @@
+package objectOrientedProgramming;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Numbers numbers = new Numbers();
+numbers.sayNumber(7.5); 
+numbers.sayNumberPlus2(10);
+numbers.saySum(10,10);
+
+	}
+
+}
