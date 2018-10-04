91c0a65dbd57beea19b0ca819fde706ab8366fb9
Daisy <19chend@elmbrookstudents.org>
2018-10-04 14:09:26 -0500

    Home improvement exercise

--- src/objectOrientedProgramming/Numbers.java
+++ src/objectOrientedProgramming/Numbers.java
@@ -14,5 +14,20 @@ public void sayNumberPlus2(double num){
 public void saySum(double num1, double num2){
 System.out.println("The sum of your numbers is "+(num1+num2));
 }
+public double returnSquare(double num){
+	num = Math.pow(num,2.0);
+	return num;
 }
+public double returnArea(double num1, double num2){
+	double area = -999.0;
+	area = num1*num2;
+	return area;
+}
+public int returnRoundUp(double num){
+	num = (int)Math.ceil(num);
+	return (int)num;
+}
+	
+}
+
 
--- src/objectOrientedProgramming/NumbersTest.java
+++ src/objectOrientedProgramming/NumbersTest.java
@@ -8,7 +8,13 @@ Numbers numbers = new Numbers();
 numbers.sayNumber(7.5); 
 numbers.sayNumberPlus2(10);
 numbers.saySum(10,10);
-
+double answer = numbers.returnSquare(10.0);
+	System.out.println("The square of the value is "+answer);
+double area = numbers.returnArea(10, 10);
+System.out.println("The area of the rectangle is "+area);
+double num = numbers.returnRoundUp(15.5);
+System.out.println("The next largest integer is "+num);
 	}
+
 
 }
