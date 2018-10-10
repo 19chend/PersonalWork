4ccf56fd444d7e4c16763d0ff5a7b1edf489db7c
Daisy <19chend@elmbrookstudents.org>
2018-10-10 13:49:46 -0500

    Greeter

--- src/objectOrientedProgramming/Greeter.java
+++ src/objectOrientedProgramming/Greeter.java
@@ -2,24 +2,17 @@ package objectOrientedProgramming;
 //import statements at the top
 public class Greeter {
 //instance fields - data storage
-	private int age;
+	
 //constructors - methods for constructing the object
 	
 //default constructor
 public Greeter()
 {
 	//statements to initialize the state of the object
-	age = 17;
 }
 //methods - things the object can do
 public void sayHello()
 {
 	System.out.println("Hello");
-}
-public int getAge(){
-	return age;
-}
-public void setAge(int newAge){
-	age=newAge;
 }
 }
