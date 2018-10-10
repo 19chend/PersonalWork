a87790ea13c542f1ee1ddfe72b6f6d790c8dbd39
Daisy <19chend@elmbrookstudents.org>
2018-10-10 14:52:00 -0500

    Circle class

---
+++ src/objectOrientedProgramming/Circle.java
@@ -0,0 +1,28 @@
+package objectOrientedProgramming;
+
+public class Circle {
+private double radius;
+public Circle(){
+	radius = -999.0;
+}
+public Circle(double radius){
+}
+public double returnRadius(){
+	return radius;
+	
+}
+public double circumference(){
+	double circumference = -999.0;
+	circumference = radius*2*Math.PI;
+	return circumference;
+}
+public double area(){
+	double area = -999.0;
+	area = Math.PI*radius*radius;
+	return area;
+}
+public void changeRadius(){
+	double newRadius = -999.0;
+	radius = newRadius;
+}
+}
