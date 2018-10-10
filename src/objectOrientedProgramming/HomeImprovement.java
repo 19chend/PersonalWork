41d9b30300e8bc9ec8834737b50d03859baccce0
Daisy <19chend@elmbrookstudents.org>
2018-10-10 15:04:40 -0500

    Home improvement exercise

--- src/objectOrientedProgramming/HomeImprovement.java
+++ src/objectOrientedProgramming/HomeImprovement.java
@@ -1,7 +1,42 @@
 package objectOrientedProgramming;
 
+import java.util.Scanner;
+
 public class HomeImprovement {
 public HomeImprovement(){
 }
-
+public void paintAWall(){
+Scanner input = new Scanner(System.in);
+double length = -999.0;
+double height = -999.0;
+double area = -999.0;
+double time = -999.0;
+System.out.println("What is the length of the wall in ft?");
+length = input.nextDouble();
+System.out.println("What is the height of the wall in ft?");
+height = input.nextDouble();
+area = length*height;
+time = area*60.0/200.0;
+System.out.println("It will take "+time+" mintes to paint the wall.");
+}
+public void plantTheGarden(){
+Scanner input = new Scanner(System.in);
+final double WIDTH = 10.0;
+double length = -999.0;
+double area = -999.0;
+double time = -999.0;
+System.out.println("What is the length of the garden in ft?");
+length = input.nextDouble();
+area = length * WIDTH;
+time = area/7.5;
+System.out.println("It will take "+time+" hours to plant the garden.");
+}
+public void fillThePool(double radius, double height){
+double area = -999.0;
+double time = -999.0;
+area = (radius*radius*Math.PI)*height;
+area = area * 7.48052;
+time = area/300.0;
+System.out.println("It will take "+time+" hours to fill the pool.");
+}
 }
--- src/objectOrientedProgramming/HomeImprovementTest.java
+++ src/objectOrientedProgramming/HomeImprovementTest.java
@@ -5,15 +5,15 @@ public class HomeImprovementTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner input = new Scanner(System.in);
-double length = -999.0;
+HomeImprovement bob = new HomeImprovement();
+bob.paintAWall();
+bob.plantTheGarden();
+System.out.println("What is the radius and height of the pool in RR HH form?(ft)");
+double radius = -999.0;
 double height = -999.0;
-double area = -999.0;
-System.out.println("What is the length of the wall in ft?");
-length = input.nextDouble();
-System.out.println("What is the height of the wall in ft?");
+radius = input.nextDouble();
 height = input.nextDouble();
-
-
+bob.fillThePool(radius, height);
 	}
 
 }
