581f15c6f493b3a0617cb33effaa427a389c5fcb
Daisy <19chend@elmbrookstudents.org>
2018-10-11 14:02:28 -0500

    Cat incomplete

--- src/objectOrientedProgramming/Cat.java
+++ src/objectOrientedProgramming/Cat.java
@@ -4,17 +4,26 @@ public class Cat {
 private int age;
 private int sleep;
 public Cat() {
+	age = -999;
+	sleep = -999;
 }
-public void birthday() {
-	age = age++;
+public int getAge(){
+	return age;
 }
-public int sleepMore() {
-	sleep = sleep++;
+public int getSleep() {
 	return sleep;
 }
-public int sleepLess() {
-	sleep=sleep--;
-	return sleep;
+public int getBirthday() {
+	age++;
+	return age;
 }
-
+public void setAge(int newAge){
+	age = newAge;
+}
+public void setSleepMore(int sleepMore){
+	sleep = sleep+1;
+}
+public void setSleepLess(int sleepLess){
+	sleep = sleep-1;
+}
 }
