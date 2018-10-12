f86bf0ca51372cc7e5c36f94e2a3e1e3ce20c596
Daisy <19chend@elmbrookstudents.org>
2018-10-12 13:15:50 -0500

    Isosceles trapezoid exercise

---
+++ src/objectOrientedProgramming/IsoscelesTrapezoidTest.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class IsoscelesTrapezoidTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+IsoscelesTrapezoid sab = new IsoscelesTrapezoid();
+sab = new IsoscelesTrapezoid(10, 12, 5, 6);
+System.out.println("Perimemter: "+sab.getPerimeter());
+System.out.println("Area: "+sab.getArea());
+	}
+
+}
